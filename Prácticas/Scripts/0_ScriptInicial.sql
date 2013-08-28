f23aa29ce436aa13ff0b8b66efa73d15c05f4ec6
fei.uv.lgmj@gmail.com <luisg.montanej@gmail.com>
2013-08-28 01:05:49 -0500

    Carpeta con scripts.

---
+++ Prácticas/Scripts/0_ScriptInicial.sql
@@ -0,0 +1,10 @@
+ALTER LOGIN sa WITH PASSWORD = 'tsci@@~~';
+ALTER LOGIN tsci WITH PASSWORD = 'tsci@@~~';
+
+CREATE LOGIN tsci WITH password = 'tsci@@~~';
+CREATE USER tsci for Login tsci;
+GO
+
+--Convertirlo Admin
+EXEC master..sp_addsrvrolemember @loginame = N'tsci', @rolename = N'sysadmin';
+GO 
