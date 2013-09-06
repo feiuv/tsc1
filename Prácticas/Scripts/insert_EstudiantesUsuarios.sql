2a1cde911023abcfff3191bfd4348779c3e58079
fei.uv.lgmj@gmail.com <luisg.montanej@gmail.com>
2013-09-06 13:51:42 -0500

    DB Scripts

--- Prácticas/Scripts/insert_EstudiantesUsuarios.sql
+++ Prácticas/Scripts/insert_EstudiantesUsuarios.sql
@@ -1,14 +1,13 @@
 USE [TSCI]
 GO
-
+DELETE FROM [dbo].[Estudiantes];
 DELETE FROM [dbo].[Usuarios];
+
 SET IDENTITY_INSERT [dbo].[Usuarios] ON 
-INSERT INTO [dbo].[Usuarios]  ([Id],[Password]) VALUES (1, 'admin' ,'123456');
-INSERT INTO [dbo].[Usuarios]  ([Id], [Usuario],[Password]) VALUES (2, 'hugo' ,'123456');
+INSERT INTO [dbo].[Usuarios]  ([Id], [Usuario], [Password]) VALUES (1, 'admin' ,'123456');
+INSERT INTO [dbo].[Usuarios]  ([Id], [Usuario], [Password]) VALUES (2, 'hugo' ,'123456');
 SET IDENTITY_INSERT [dbo].[Usuarios] OFF
 
-
-DELETE FROM [dbo].[Estudiantes];
 SET IDENTITY_INSERT [dbo].[Estudiantes] ON 
 INSERT INTO [dbo].[Estudiantes] ([Id], [Nombre], [ApP], [ApM], [Matricula], [Edad], [IdUsuario]) VALUES (1, 'Luis', 'Montané', 'Jiménez', 'S03005434', 27, 1);
 INSERT INTO [dbo].[Estudiantes] ([Id], [Nombre], [ApP], [ApM], [Matricula], [Edad], [IdUsuario]) VALUES (2, 'Gerardo', 'Montero', 'López', 'S03005414', 37, 1);
