f208be614d7be4ead9aef066f9d80f8cf98d2b6c
fei.uv.lgmj@gmail.com <luisg.montanej@gmail.com>
2013-11-23 19:25:26 -0600

    Corecciones Proyecto Móvil

---
+++ Prácticas/Práctica12 - TestDemoPhoneApp1/TestDemoPhoneApp1/Model/Estudiante.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDemoPhoneApp1.Model
+{
+    public class Estudiante
+    {
+        public string apellidomaterno { get; set; }
+        public string apellidopaterno { get; set; }
+        public int edad { get; set; }
+        public string id { get; set; }
+        public string nombre { get; set; }
+    }
+}
